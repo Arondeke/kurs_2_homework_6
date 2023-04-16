68923299d45baa0a341ff41648778601dc4f14d6
Arondeke <khmelnitskiy.aleksey@gmail.com>
2023-04-16 23:16:38 +0300

    //test

---
+++ src/main/java/com/kurs2/hw6/Employee.java
@@ -0,0 +1,11 @@
+package com.kurs2.hw6;
+
+public class Employee {
+    private final String firstName;
+    private final String lastName;
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
